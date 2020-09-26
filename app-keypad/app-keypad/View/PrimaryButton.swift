c71f428af30c5587a2490e0ca333bc9c6f0abca9
Julián Miño <46285542+julianmino@users.noreply.github.com>
2020-09-26 02:57:54 -0300

    Did some Design Changes
    Added PrimaryButton Class.
    Designed MainVC.

---
+++ app-keypad/app-keypad/View/PrimaryButton.swift
@@ -0,0 +1,36 @@
+//
+//  PrimaryButton.swift
+//  app-keypad
+//
+//  Created by Julian Mino on 26/09/2020.
+//  Copyright © 2020 Julian Mino. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class PrimaryButton: UIButton {
+
+    
+    override func prepareForInterfaceBuilder() {
+        customDesign()
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customDesign()
+    }
+
+    func customDesign() {
+        layer.backgroundColor = #colorLiteral(red: 0.05843322724, green: 0.08758834749, blue: 0.1923964024, alpha: 1)
+        layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        layer.borderWidth = 2.0
+        layer.opacity = 1
+        layer.cornerRadius = 5.0
+        
+        setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        titleLabel?.textAlignment = .center
+        
+        
+    }
+    
+    
+}
